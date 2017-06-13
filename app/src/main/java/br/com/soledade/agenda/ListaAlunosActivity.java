2b8b166c0119c921e6e9a4e61781be8ca1490e8d
Micael Souza <micael.ns@hotmail.com>
2017-06-13 15:02:07 -0300

    implementado, acesso a SMS, navegador , e browser

--- app/src/main/java/br/com/soledade/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/soledade/agenda/ListaAlunosActivity.java
@@ -1,6 +1,9 @@
 package br.com.soledade.agenda;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -13,6 +16,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.List;
+import java.util.jar.Manifest;
 
 import br.com.soledade.agenda.dao.AlunoDAO;
 import br.com.soledade.agenda.modelo.Aluno;
@@ -77,13 +81,50 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v,final ContextMenu.ContextMenuInfo menuInfo) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        final Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
+
+        MenuItem itemLigar = menu.add("Ligar");
+        itemLigar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                /*if(ActivityCompat.checkSelfPermission(ListaAlunosActivity.this, Manifest.permission.CALL_PHONE)
+                        != PackageManager.PERMISSION_GRANTED){
+                        ActivityCompat.requestPermissions(ListaAlunosActivity.this,
+                                new String[]{Manifest.permission.CALL_PHONE},123);
+                }else {*/
+                        Intent intentLigar = new Intent(Intent.ACTION_VIEW);
+                        intentLigar.setData(Uri.parse("tel:" + aluno.getTelefone()));
+                        startActivity(intentLigar);
+//                }
+                return false;
+            }
+        });
+
+        MenuItem itemSMS = menu.add("ENVIAR SMS");
+        Intent intentSMS = new Intent(Intent.ACTION_VIEW);
+        intentSMS.setData(Uri.parse("sms:"+aluno.getTelefone()));
+        itemSMS.setIntent(intentSMS);
+
+        MenuItem itemMAPA = menu.add("Visualizar no mapa");
+        Intent intentMAPA = new Intent(Intent.ACTION_VIEW);
+        intentMAPA.setData(Uri.parse("geo:0,0?q="+aluno.getEndereco()));
+        itemMAPA.setIntent(intentMAPA);
+
+        MenuItem itemSite = menu.add("Visitar site");
+        Intent intentSite = new Intent(Intent.ACTION_VIEW);
+        String site= aluno.getSite();
+        if(!site.startsWith("http://")){
+            site= "http://"+site;
+        }
+        intentSite.setData(Uri.parse(site));
+        itemSite.setIntent(intentSite);
+
         MenuItem deletar=menu.add("Deletar");
         deletar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
-                Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
-                AlunoDAO dao=new AlunoDAO(ListaAlunosActivity.this);
+                 AlunoDAO dao=new AlunoDAO(ListaAlunosActivity.this);
                 dao.deleta(aluno);
                 dao.close();
                 carregaAlunos();
